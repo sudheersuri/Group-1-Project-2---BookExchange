f863cc6a7be7f7cf0b42c269ac3dd3f6b308dfdf
Man1koy <mansikoyande5@gmail.com>
2020-12-21 13:07:09 -0500

    Updated the Forgot.js and EditProfile.js files
    Updated the Forgot.js and EditProfile.js files

--- group-1-project-2/client/src/components/EditProfile.js
+++ group-1-project-2/client/src/components/EditProfile.js
@@ -1,14 +1,14 @@
-import Axios from "axios";
-import React, { useState, useEffect } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
-export default function EditProfile() {
-  const [redirect, setRedirect] = useState("");
+import Axios from "axios";                                                   //axios is a open source library we have imported for http requests and fetching. 
+import React, { useState, useEffect } from "react";                          //we have imported the react library
+import { Button, Col, Container, Form, Row } from "react-bootstrap";         // We have imported Button, Col, Container, Form, Row from the react-bootsrap library
+import { useNavigate } from "react-router-dom";                              // This imports the navigation feature for the user from the react-router-dom
+export default function EditProfile() {                                      //this exports the Edit profile function
+  const [redirect, setRedirect] = useState("");                              // this sets the current stae to redirect
 
-  if (!localStorage.getItem("uid")) {
+  if (!localStorage.getItem("uid")) {                                        //if the user id is not equal to the local storage it redirects the user to the login page
     window.location.href = "/login";
   }
-
+  // else it returns the message, counter as per the current state of the user
   const [response, setResponse] = useState("");
 
   const [message, setMesssage] = useState("");
@@ -16,36 +16,36 @@ export default function EditProfile() {
   const [counter, setCounter] = useState("");
   //part 2
   useEffect(() => {
-    Axios.post("http://localhost:4000/profile", {
+    Axios.post("http://localhost:4000/profile", {                          // this posts the user id from the local storage to the url localhost:4000/profile page in the user id field
       uid: localStorage.getItem("uid"),
     }).then((response) => {
-      if (typeof response.data.error !== "undefined") {
+      if (typeof response.data.error !== "undefined") {                   // if the user is undefined it returns the error message and redirects the user to the home page
         // redirect to home
         setMesssage(response.data.error);
       } else {
-        setResponse(response.data.details[0]);
+        setResponse(response.data.details[0]);                            // accepts the details of the user
       }
     });
   }, [counter]);
-  const onFormSubmit = (e) => {
+  const onFormSubmit = (e) => {                                          // function that executes on submission of the form
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const formDataObj = Object.fromEntries(formData.entries());
-    formDataObj.uid = localStorage.getItem("uid");
-    Axios.post("http://localhost:4000/updateuser", formDataObj).then(
+    const formData = new FormData(e.target);                             // new form data generated on the target page
+    const formDataObj = Object.fromEntries(formData.entries());          // form data is taken as entries from the user in the form object form
+    formDataObj.uid = localStorage.getItem("uid");                       // gets the user id from local storage stored as a form object entry
+    Axios.post("http://localhost:4000/updateuser", formDataObj).then(    // posts the data on the page with url localhost:4000/updateuser page
       (response) => {
-        if (typeof response.data.error !== "undefined") {
+        if (typeof response.data.error !== "undefined") {                // if the user is undefined it gives the error message and redirects the user to the home page.
           //redirect to home
           setMesssage(response.data.error);
         } else {
-          localStorage.setItem("username", formDataObj.uname);
-          setMesssage(response.data.message);
-          setCounter(counter + 1);
+          localStorage.setItem("username", formDataObj.uname);          //sets the username entered by the user in the form object as uname and stored in local storage
+          setMesssage(response.data.message);                           //gives a message
+          setCounter(counter + 1);                                      // sets the counter and increments the counter by 1
         }
       }
     );
   };
-  return (
+  return (                                                             //this function returns the form with the Fields Username to be neterd by the user and Email address field for email id to be entered by the user and a submit button to submit the data entered in these feilds by the user.
     <div style={{ padding: 20 }}>
       <Container>
         <Row className="justify-content-md-center">
--- group-1-project-2/client/src/components/Forgot.js
+++ group-1-project-2/client/src/components/Forgot.js
@@ -1,25 +1,25 @@
-import Axios from "axios";
-import React, { useState } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
-export default function Forgot() {
-  const [message, setMesssage] = useState("");
+import Axios from "axios";                                                         //axios is a open source library we have imported for http requests and fetching.
+import React, { useState } from "react";                                           //we have imported the react library
+import { Button, Col, Container, Form, Row } from "react-bootstrap";               // we have imported the react bootstrap library 
+export default function Forgot() {                                                 // this exports the forgot function
+  const [message, setMesssage] = useState("");                                    // returns the current state
   const onFormSubmit = (e) => {
-    const formData = new FormData(e.target);
-    const formDataObj = Object.fromEntries(formData.entries());
-    Axios.post("http://localhost:4000/sendforgetemail", formDataObj).then(
+    const formData = new FormData(e.target);                                      // returns the new form data to the targeted page
+    const formDataObj = Object.fromEntries(formData.entries());                   // returns the form entries
+    Axios.post("http://localhost:4000/sendforgetemail", formDataObj).then(        // this posts the data to localhost:4000/sendforgetemail page
       (response) => {
-        if (typeof response.data.error !== "undefined") {
-          //redirect to home
+        if (typeof response.data.error !== "undefined") {                        // gives the error message if the user is undefined and redirects user to the home page
+          //redirect to home 
           setMesssage(response.data.error);
         } else {
-          setMesssage(response.data.message);
+          setMesssage(response.data.message);                                   // if user is valid it gives the correct message
         }
       }
     );
     e.preventDefault();
   };
 
-  return (
+  return (                                                                     // This function returns the Form structured page having email feild in the form and a text below this feild "We'll never share your email with anyone else" and a submit button below it to submit the email id.
     <div style={{ padding: 20 }}>
       <Container>
         <Row className="justify-content-md-center">
